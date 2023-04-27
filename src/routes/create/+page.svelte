d058e2e17a1bdc701e54ffff97b1dc28fb6aa88a
Tristan White <tristanwhite.gva@gmail.com>
2023-04-27 13:11:03 -0500

    refactor: Added errors and constraints

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -7,24 +7,33 @@
     export let data: PageData;
 
     // Client API:
-    const { form } = superForm(data.form);
+    const { form, errors, constraints } = superForm(data.form);
 </script>
 
 <SuperDebug data={$form} />
 
 <form method="POST">
     <label for="name">Name
-        <input class="input" type="text" name="name" bind:value={$form.name} />
+        <input class="input" type="text" name="name" bind:value={$form.name} data-invalid={$errors.name} {...$constraints.name}/>
+        {#if $errors.name}<span class="invalid">{$errors.name}</span>{/if}
     </label>
     
     <label for="email">E-mail
-        <input class="input" type="email" name="email" bind:value={$form.email} />
+        <input class="input" type="email" name="email" bind:value={$form.email} data-invalid={$errors.email} {...$constraints.email}/>
+        {#if $errors.name}<span class="invalid">{$errors.email}</span>{/if}
     </label>
     
     <label for="email">Content
-        <input class="textarea" type="text" name="content" bind:value={$form.content} />
+        <textarea class="textarea" rows="4" name="content" bind:value={$form.content} data-invalid={$errors.content} {...$constraints.content}/>
+        {#if $errors.name}<span class="invalid">{$errors.content}</span>{/if}
     </label>
     
 
     <div><button>Submit</button></div>
 </form>
+
+<style>
+    .invalid {
+      color: red;
+    }
+  </style>
